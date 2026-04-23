2337bd35b6fd20ff4aaff5769ec08bc8ec4276d3
sshubham78 <166130264+sshubham78@users.noreply.github.com>
2026-04-23 07:21:54 +0530

    Implement PDF generation for bank statements
    Added functionality to generate and download a PDF bank statement.

--- app.py
+++ app.py
@@ -1,1 +1,36 @@
+from flask import Flask, render_template, request, send_file
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
+from reportlab.lib.styles import getSampleStyleSheet
+import os
 
+app = Flask(__name__)
+
+def generate_pdf(name, account):
+    filename = "statement.pdf"
+    doc = SimpleDocTemplate(filename)
+    styles = getSampleStyleSheet()
+
+    elements = []
+    elements.append(Paragraph("Bank Statement", styles['Title']))
+    elements.append(Spacer(1, 12))
+    elements.append(Paragraph(f"Name: {name}", styles['Normal']))
+    elements.append(Paragraph(f"Account No: {account}", styles['Normal']))
+
+    doc.build(elements)
+    return filename
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    name = request.form['name']
+    account = request.form['account']
+
+    file = generate_pdf(name, account)
+    return send_file(file, as_attachment=True)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
